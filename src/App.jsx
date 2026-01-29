c63a3db35c917585e583d118af2654e5787a28b9
Kacper <programming.r24pp@pm.me>
2026-01-29 19:15:44 +0100

    feat: handle basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Stocks from './pages/Stocks'
+import Currencies from './pages/Currencies'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
+import Wallet from './pages/Wallet'
 
 const App = () => {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/stocks' element={<Stocks />} />
+        <Route path='/currencies' element={<Currencies />} />
+        <Route path='/wallet' element={<Wallet />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
