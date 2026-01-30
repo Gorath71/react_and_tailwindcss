7745082bc97b0dfb2ae11a2073c54e0b332ea5fe
Kacper <programming.r24pp@pm.me>
2026-01-30 18:39:58 +0100

    feat: add footer

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <h3>NotFound</h3>
+    <main className='flex justify-center p-4'>
+      <div className='w-full max-w-screen-xl flex flex-col gap-2 pt-32 pb-32 text-center'>
+        <h3>404 – Page not found</h3>
+        <span className='text-accent'>The page you are looking for doesn’t exist or has been moved.</span>
+      </div>
+    </main>
   )
 }
 
